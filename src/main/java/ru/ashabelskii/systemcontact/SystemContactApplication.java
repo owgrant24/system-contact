c605052d2a3581ee8ce85f88cd971c3a20f6f784
ashabelskii <owgrant18@gmail.com>
2024-05-19 23:27:10 +0300

    project init

---
+++ src/main/java/ru/ashabelskii/systemcontact/SystemContactApplication.java
@@ -0,0 +1,14 @@
+package ru.ashabelskii.systemcontact;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class SystemContactApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SystemContactApplication.class, args);
+    }
+
+}
